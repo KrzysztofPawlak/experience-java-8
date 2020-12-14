8e6f1b00b1284a2a5dcaf4fa0bcd7f1375397e51
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2020-12-14 14:02:12 +0100

    data_and_time: shouldThrowParseException

--- src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
+++ src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
@@ -57,8 +57,8 @@ public class DateAndTimeTest {
 
     @Test(expected = DateTimeParseException.class)
     public void shouldThrowParseException(){
-        String newYearsEveAsString = null;
-        LocalDate newYearsEve = null; // parse the an error
+        String newYearsEveAsString = "2017-12-31";
+        LocalDate newYearsEve = LocalDate.parse(newYearsEveAsString, DateTimeFormatter.ofPattern("dd-mm-yyyy")); // parse the an error
     }
 
     @Test
