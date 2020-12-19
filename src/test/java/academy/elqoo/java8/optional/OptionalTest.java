c85e5087f3c865e6482b1af8a98a577f724e3cd9
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2020-12-19 11:09:59 +0100

    optional: shouldReturnBookName

--- src/test/java/academy/elqoo/java8/optional/OptionalTest.java
+++ src/test/java/academy/elqoo/java8/optional/OptionalTest.java
@@ -19,7 +19,7 @@ public class OptionalTest {
     @Test
     public void shouldReturnBookName(){
         Optional<Book> book = Optional8.getBook();
-        String bookName = null; // book. ....
+        String bookName = book.get().getName(); // book. ....
         assertThat(bookName,equalTo("Experience Java 8"));
     }
 
